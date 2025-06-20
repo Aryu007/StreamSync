e852215ce4c4cfc168249c4ef459b522989bf70b
202201239 <202201239@daiict.ac.in>
2025-06-20 23:46:54 +0530

    Complete SignUp frontend part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,39 @@
 import React from 'react'
-import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage.jsx';
-import { SignUpPage } from './pages/SignUpPage.jsx';
-import { LoginPage } from './pages/LoginPage.jsx';
-import { NotificationsPage } from './pages/NotificationsPage.jsx';
-import { CallPage } from './pages/CallPage.jsx';
-import { OnboardingPage } from './pages/OnboardingPage.jsx';
-import { ChatPage } from './pages/ChatPage.jsx';
+import { Routes, Route, Navigate } from 'react-router';
+import  HomePage  from './pages/HomePage.jsx';
+import  SignUpPage  from './pages/SignUpPage.jsx';
+import  LoginPage  from './pages/LoginPage.jsx';
+import  NotificationsPage  from './pages/NotificationsPage.jsx';
+import  CallPage  from './pages/CallPage.jsx';
+import  OnboardingPage  from './pages/OnboardingPage.jsx';
+import  ChatPage  from './pages/ChatPage.jsx';
 import { Toaster } from 'react-hot-toast';
 import { axiosInstance } from './lib/axios.js';
 import { useQuery } from '@tanstack/react-query';
 
 const App = () => {
   //Tanstack Query
-  const {data, isLoading, error} = useQuery({
-    queryKey: ['exampleData'],
+  const {data: authData, isLoading, error} = useQuery({
+    queryKey: ['authUser'],
     queryFn: async () => {
       const response = await axiosInstance.get('/auth/me');
        return response.data;
     },
+    retry: false, // Disable retry on failure
   });
-  console.log(data, isLoading, error);
+  
+  const authUser = authData?.user;
+
   return (
     <div className="App">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/notifications" element ={<NotificationsPage />}/>
-          <Route path="/call" element={<CallPage />} />
-          <Route path="/onboarding" element={<OnboardingPage />} />
-          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/" element={authUser ? <HomePage /> : <Navigate to={'/login'}/>} />
+          <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={'/'}/>} />
+          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={'/'}/>} />
+          <Route path="/notifications" element ={authUser ? <NotificationsPage /> : <Navigate to={'/login'}/>}/>
+          <Route path="/call" element={authUser ? <CallPage /> : <Navigate to={'/login'}/>} />
+          <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to={'/login'}/>} />
+          <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to={'/login'}/>} />
         </Routes>
         <Toaster/>
     </div>
