34ab3cb13ea7b4aba556e1b809dfce99cb4b31e8
202201239 <202201239@daiict.ac.in>
2025-06-28 23:55:16 +0530

    Final changes

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -50,7 +50,7 @@ export async function sendFriendRequest(userId) {
 }
 
 export async function getFriendRequests() {
-  const response = await axiosInstance.get("/users/friend-requests");
+  const response = await axiosInstance.get("/users/friendsrequests");
   return response.data;
 }
 
