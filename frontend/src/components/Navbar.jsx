e5b870b3c1461faa1b92f00b50d6d8cc1bb3b5a7
202201239 <202201239@daiict.ac.in>
2025-06-28 23:35:27 +0530

    Final changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import useAuthUser from "../hooks/useAuthUser";
-import { useLocation } from "react-router";
-import { Laptop , BellIcon, LogOutIcon} from "lucide-react";
-import { Link } from "react-router";
-import useLogout from "../hooks/useLogout.js";
+import { Link, useLocation } from "react-router";
+import useAuthUser from "../hooks/useAuthUser.js";
+import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector.jsx";
+import useLogout from "../hooks/useLogout.js";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
-  const isChatPage = location.pathname.startsWith("/chat");
+  const isChatPage = location.pathname?.startsWith("/chat");
 
-  const {logoutMutation } = useLogout();
+  // const queryClient = useQueryClient();
+  // const { mutate: logoutMutation } = useMutation({
+  //   mutationFn: logout,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  const { logoutMutation } = useLogout();
 
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
@@ -20,7 +25,7 @@ const Navbar = () => {
           {isChatPage && (
             <div className="pl-5">
               <Link to="/" className="flex items-center gap-2.5">
-                <Laptop className="size-9 text-primary" />
+                <ShipWheelIcon className="size-9 text-primary" />
                 <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
                   StreamSync
                 </span>
@@ -41,11 +46,7 @@ const Navbar = () => {
 
           <div className="avatar">
             <div className="w-9 rounded-full">
-              <img
-                src={authUser?.profilePic}
-                alt="User Avatar"
-                rel="noreferrer"
-              />
+              <img src={authUser?.profilePic} alt="User Avatar" rel="noreferrer" />
             </div>
           </div>
 
@@ -58,5 +59,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,18 +1,19 @@
-import useAuthUser from "../hooks/useAuthUser.js";
-import { useLocation, Link } from "react-router";
-import { Laptop, HomeIcon, BellIcon } from "lucide-react";
+import { Link, useLocation } from "react-router";
+import useAuthUser from "../hooks/useAuthUser";
+import { BellIcon, HomeIcon, ShipWheelIcon, UsersIcon } from "lucide-react";
 
 const Sidebar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
   const currentPath = location.pathname;
+
   return (
     <aside className="w-64 bg-base-200 border-r border-base-300 hidden lg:flex flex-col h-screen sticky top-0">
       <div className="p-5 border-b border-base-300">
         <Link to="/" className="flex items-center gap-2.5">
-          <Laptop className="size-9 text-primary" />
+          <ShipWheelIcon className="size-9 text-primary" />
           <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-            StreamSync
+            Streamify
           </span>
         </Link>
       </div>
@@ -28,6 +29,15 @@ const Sidebar = () => {
           <span>Home</span>
         </Link>
 
+        <Link
+          to="/friends"
+          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
+            currentPath === "/friends" ? "btn-active" : ""
+          }`}
+        >
+          <UsersIcon className="size-5 text-base-content opacity-70" />
+          <span>Friends</span>
+        </Link>
 
         <Link
           to="/notifications"
@@ -45,7 +55,7 @@ const Sidebar = () => {
         <div className="flex items-center gap-3">
           <div className="avatar">
             <div className="w-10 rounded-full">
-              <img src={authUser?.profilePicture} alt="User Avatar" />
+              <img src={authUser?.profilePic} alt="User Avatar" />
             </div>
           </div>
           <div className="flex-1">
@@ -60,5 +70,4 @@ const Sidebar = () => {
     </aside>
   );
 };
-
 export default Sidebar;
